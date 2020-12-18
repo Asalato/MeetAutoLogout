5c47092b6ba375be3bbc322afd201d757f4dde2b
MitaMasaki <mimimita3131@gmail.com>
2020-12-18 23:07:52 +0900

    Update logoutTimer.js
    Added the ability to log out under a certain number of people.

--- logoutTimer.js
+++ logoutTimer.js
@@ -31,6 +31,13 @@ GM_addStyle ( `
         cursor: pointer;
         pointer-events: auto;
     }
+    #number {
+        width: 100%;
+        font-size: xx-large;
+        text-align: center;
+        cursor: pointer;
+        pointer-events: auto;
+    }
     #startButton {
         position: absolute;
         top: 100%;
@@ -47,40 +54,96 @@ GM_addStyle ( `
         cursor: pointer;
         pointer-events: auto;
     }
+    #timeButton {
+        position: absolute;
+        top: 0%;
+        left: -12.3%;
+        width: 42%;
+        cursor: pointer;
+        pointer-events: auto;
+        text-align: center;
+    }
+    #numberButton {
+        position: absolute;
+        top: 50%;
+        left: -12.3%;
+        width: 42%;
+        cursor: pointer;
+        pointer-events: auto;
+        text-align: center;
+    }
 ` );
 
-const timerNode = document.createElement('div');
-timerNode.setAttribute ('id', 'logoutTimer');
-const timerInputNode = document.createElement('input');
-timerInputNode.setAttribute('type', 'time');
-timerInputNode.setAttribute('id', 'timer');
-timerNode.appendChild(timerInputNode);
+let isTimer = true;
 
-const startNode = document.createElement('div');
-startNode.setAttribute ('id', 'startButton');
-const startButtonNode = document.createElement('button');
-startButtonNode.setAttribute('id', 'startButton');
-startButtonNode.setAttribute('disabled', 'true');
-startButtonNode.innerHTML = 'Start';
-startNode.appendChild(startButtonNode);
-timerNode.appendChild(startNode);
+const tn = document.createElement('div');
+tn.setAttribute ('id', 'logoutTimer');
+const tin = document.createElement('input');
+tin.setAttribute('type', 'time');
+tin.setAttribute('id', 'timer');
+tn.appendChild(tin);
 
-const stopNode = document.createElement('div');
-stopNode.setAttribute ('id', 'stopButton');
-const stopButtonNode = document.createElement('button');
-stopButtonNode.setAttribute('id', 'stopButton');
-stopButtonNode.setAttribute('disabled', 'true');
-stopButtonNode.innerHTML = 'Stop';
-stopNode.appendChild(stopButtonNode);
-timerNode.appendChild(stopNode);
+const nin = document.createElement('input');
+nin.setAttribute('type', 'number');
+nin.setAttribute('id', 'number');
+nin.setAttribute('min', '1');
+nin.defaultValue = '1';
+
+const stan = document.createElement('div');
+stan.setAttribute ('id', 'startButton');
+const stabn = document.createElement('button');
+stabn.setAttribute('id', 'startButton');
+stabn.innerHTML = 'Start';
+stabn.disabled = true;
+stan.appendChild(stabn);
+tn.appendChild(stan);
+
+const ston = document.createElement('div');
+ston.setAttribute ('id', 'stopButton');
+const stobn = document.createElement('button');
+stobn.setAttribute('id', 'stopButton');
+stobn.disabled = true;
+stobn.innerHTML = 'Stop';
+ston.appendChild(stobn);
+tn.appendChild(ston);
+
+const tsn = document.createElement('div');
+tsn.setAttribute('id', 'timeButton');
+const tsbn = document.createElement('button');
+tsbn.setAttribute('id', 'timeButton');
+tsbn.disabled = true;
+tsbn.innerHTML = '⏲';
+tsn.appendChild(tsbn);
+tn.appendChild(tsn);
+
+const nsn = document.createElement('div');
+nsn.setAttribute('id', 'numberButton');
+const nsbn = document.createElement('button');
+nsbn.setAttribute('id', 'numberButton');
+nsbn.innerHTML = '👥';
+nsn.appendChild(nsbn);
+tn.appendChild(nsn);
 
 let targetTime = '';
-timerInputNode.addEventListener('input', function(eve) {
+tin.addEventListener('input', function(eve) {
     let val = eve.target.value;
     if(val === '') return;
     targetTime = val;
-    stopButtonNode.disabled = true;
-    startButtonNode.disabled = false;
+    stobn.disabled = true;
+    stabn.disabled = false;
+});
+
+let targetNumber = 1;
+nin.addEventListener('input', function(eve){
+    let val = parseInt(eve.target.value);
+    if(val < 1) {
+        stobn.disabled = true;
+        stabn.disabled = true;
+        return;
+    }
+    targetNumber = val;
+    stobn.disabled = true;
+    stabn.disabled = false;
 });
 
 let timer;
@@ -88,40 +151,82 @@ let timeRemaining = -1;
 function countDown(){
     timeRemaining -= 1;
     if(timeRemaining > 0) return;
+    clearInterval(countDown);
     location.reload();
 }
 
-startButtonNode.onclick = function () {
-    startButtonNode.disabled = true;
-    stopButtonNode.disabled = false;
-    timerInputNode.disabled = true;
+function checkNumber(){
+    let node = document.querySelector("span.wnPUne.N0PJ8e");
+    let number = parseInt(node.innerHTML);
+    if(number === -1 || number > targetNumber) return;
+    clearInterval(checkNumber);
+    location.reload();
+}
 
-    const val = targetTime.split(':');
-    const now = new Date();
-    const nowHour = now.getHours();
-    const nowMin = now.getMinutes();
-    const nowSec = now.getSeconds();
-    const hour = val[0];
-    const min = val[1];
-    timeRemaining = (hour - nowHour) * 3600 + (min - nowMin) * 60 - nowSec;
-    if(timeRemaining < 0) timeRemaining += 86400;
+stabn.onclick = function () {
+    stabn.disabled = true;
+    stobn.disabled = false;
+    nsbn.disabled = true;
+    tsbn.disabled = true;
 
-    alert('Timer is set to ' + targetTime);
-    timer = setInterval(countDown, 1000);
+    if(isTimer) {
+        tin.disabled = true;
+        const val = targetTime.split(':');
+        const now = new Date();
+        const nowHour = now.getHours();
+        const nowMin = now.getMinutes();
+        const nowSec = now.getSeconds();
+        const hour = val[0];
+        const min = val[1];
+        timeRemaining = (hour - nowHour) * 3600 + (min - nowMin) * 60 - nowSec;
+        if (timeRemaining < 0) timeRemaining += 86400;
+
+        alert('Timer set to ' + targetTime);
+        timer = setInterval(countDown, 1000);
+    }else{
+        nin.disabled = true;
+        alert('Log out when the number of participants drops below ' + targetNumber);
+        timer = setInterval(checkNumber, 1000);
+    }
 };
-stopButtonNode.onclick = function () {
-    stopButtonNode.disabled = true;
-    startButtonNode.disabled = false;
-    timerInputNode.disabled = false;
+stobn.onclick = function () {
+    stobn.disabled = true;
+    stabn.disabled = false;
+    nsbn.disabled = !isTimer;
+    tsbn.disabled = isTimer;
+    tin.disabled = !isTimer;
+    nin.disabled = isTimer;
     clearInterval(timer);
 };
+
+nsbn.onclick = function(){
+    stobn.disabled = true;
+    stabn.disabled = targetNumber < 1;
+    nsbn.disabled = true;
+    tsbn.disabled = false;
+    nin.disabled = false;
+    isTimer = false;
+    tn.appendChild(nin);
+    tn.removeChild(tin);
+}
+
+tsbn.onclick = function() {
+    stobn.disabled = true;
+    stabn.disabled = targetTime === '';
+    nsbn.disabled = false;
+    tsbn.disabled = true;
+    tin.disabled = false;
+    isTimer = true;
+    tn.appendChild(tin);
+    tn.removeChild(nin);
+}
 
 let isFirst = true;
 setInterval(function(){
     const _container = document.querySelector('div.pHsCke');
     if (_container && isFirst) {
         isFirst = false;
-        document.querySelector('div.pHsCke').appendChild (timerNode);
+        document.querySelector('div.pHsCke').appendChild (tn);
     }
     else if(!_container && !isFirst){
         isFirst = true;
